b7d47697d1cd07bed96f4a38f3c94a302a49b01a
Juan de leon <juan.deleon.dev@gmail.com>
2026-06-20 10:04:50 -0600

    fix: using type safety for feature flags

--- basekit-fe/src/store/tenant.store.ts
+++ basekit-fe/src/store/tenant.store.ts
@@ -1,22 +1,28 @@
 import { create } from "zustand";
 
+type FeatureFlagInde = "enableFirstUserCreation";
+
+type FeatureFlags = {
+  enableFirstUserCreation: boolean;
+};
+
 export interface TenantData {
   id: string;
   slug: string;
   name: string;
-  featureFlags: Record<string, boolean>;
+  featureFlags: FeatureFlags;
   configuration: Record<string, unknown> | null;
 }
 
 interface TenantState {
   tenant: TenantData | null;
   setTenant: (tenant: TenantData) => void;
-  isFeatureEnabled: (key: string) => boolean;
+  // isFeatureEnabled: (key: string) => boolean;
 }
 
 export const useTenantStore = create<TenantState>((set, get) => ({
   tenant: null,
   setTenant: (tenant) => set({ tenant }),
-  isFeatureEnabled: (key) =>
+  isFeatureEnabled: (key: FeatureFlagInde) =>
     Boolean(get().tenant?.featureFlags?.[key] ?? false),
 }));
