a5176977ef291afe49223166679f8331cb99e808
Juan de leon <juan.deleon.dev@gmail.com>
2026-06-22 22:03:12 -0600

    feat: dockerized be

---
+++ basekit-be/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "[entrypoint] Running database migrations..."
+node node_modules/.bin/typeorm migration:run -d dist/database/datasource.js
+
+echo "[entrypoint] Starting application..."
+exec "$@"
